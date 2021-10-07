65a879b0e7fa15e5115b1724aa78c95868b06fc4
Andrius <andrius.rochalazarino@gmail.com>
2021-10-06 21:00:43 -0400

    feat: added authentication

--- back-end/getAllPatients/index.js
+++ back-end/getAllPatients/index.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql');
+const jwt = require('jwt-simple')
+
+const authSecret = "adwadawdawdawdaw1351514646"
 
 const connection = mysql.createConnection({
     host: 'medcloud.ckbdecugxjkk.us-east-1.rds.amazonaws.com',
@@ -9,21 +12,48 @@ const connection = mysql.createConnection({
 
 exports.handler = async function getAllPatients(event) {
 
-    const p = new Promise((resolve) => {
-        connection.query('SELECT * FROM Patient', function (err, results) {
-            resolve(results);
-        })
-    })
-
-    const result = await p
-
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        body: JSON.stringify({ results: result })
+    const tokenAuthorization = event.headers.Authorization
+    let logged = false
+    try {
+        if (tokenAuthorization) {
+            const token = jwt.decode(tokenAuthorization, authSecret)
+            if (new Date(token.exp * 1000) > new Date()) {
+                console.log("usuario autenticado")
+                logged = true
+            }
+        }
+    } catch (e) {
+        console.log("Não logado");
     }
-}
+
+    let result = null;
+    if (logged) {
+        const p = new Promise((resolve) => {
+            connection.query('SELECT * FROM Patient', function (err, results) {
+                resolve(results);
+            });
+        });
+
+        result = await p;
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: JSON.stringify({ results: result })
+        };
+    } else {
+        return {
+            statusCode: 401,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: JSON.stringify({ results: "Login não autorizado" })
+        };
+    }
+};

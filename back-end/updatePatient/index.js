18a0366b3b994a8ec84cef9430cc7246348ca3a1
Andrius <andrius.rochalazarino@gmail.com>
2021-10-06 21:54:17 -0400

    feat: added authentication to updatePatient backend

--- back-end/updatePatient/index.js
+++ back-end/updatePatient/index.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql');
+const jwt = require('jwt-simple')
+
+const authSecret = "adwadawdawdawdaw1351514646"
 
 const connection = mysql.createConnection({
     host: 'medcloud.ckbdecugxjkk.us-east-1.rds.amazonaws.com',
@@ -10,8 +13,24 @@ const connection = mysql.createConnection({
 exports.handler = async function updatePatient(event) {
 
     const { id, name, last_name, cpf, street, street_number, district, cep, city } = JSON.parse(event.body)
+    
+    const tokenAuthorization = event.headers.Authorization
+    let logged = false
+    try {
+        if (tokenAuthorization) {
+            const token = jwt.decode(tokenAuthorization, authSecret)
+            if (new Date(token.exp * 1000) > new Date()) {
+                logged = true
+            }
+        }
+    } catch (e) {
+        console.log("Não logado");
+    }
+    
+    let result = null;
 
-    const p = new Promise((resolve) => {
+    if (logged){
+        const p = new Promise((resolve) => {
         connection.query(`UPDATE Patient SET name = "${name}",
         last_name = "${last_name}",
         cpf = "${cpf}",
@@ -38,5 +57,16 @@ exports.handler = async function updatePatient(event) {
             "X-Requested-With": "*"
         },
         body: JSON.stringify({ results: result })
+    }
+    }else{
+        return {
+            statusCode: 401,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: JSON.stringify({ results: "Login não autorizado" })
+        };
     }
 }
